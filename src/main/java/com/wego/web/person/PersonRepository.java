f580813e3319ab03d4cecb5564b4a150e58ec116
parkjungkwan <pakjkwan@gmail.com>
2019-12-27 10:05:11 +0900

    10:05

--- src/main/java/com/wego/web/person/PersonRepository.java
+++ src/main/java/com/wego/web/person/PersonRepository.java
@@ -1,5 +1,8 @@
 package com.wego.web.person;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface PersonRepository extends CrudRepository<Person, Long> {
 	public Person findByUseridAndPasswd(String userid, String passwd);
 	public Person findByUserid(String userid);
+	@Query(value="SELECT * FROM PERSON GROUP BY HAK", nativeQuery=true)
+	public List<Person> findGroupByHak();
 }
--- vue/src/components/student/StudentsFindSome.vue
+++ vue/src/components/student/StudentsFindSome.vue
@@ -1,5 +1,16 @@
 <template> 
-	<div>학생들 목록 
+	<div class="login-form">
+    <form>
+        <h2 class="text-center">검색</h2>       
+        <div class="form-group">
+            <input v-model="searchWord" type="text" class="form-control" placeholder="Username" required="required">
+        </div>
+        <div class="form-group">
+            <button @click.prevent="find" type="submit" class="btn btn-primary btn-block">전 송</button>
+        </div>
+    </form>
+	<!--
+	<div>학생 조건 검색결과  
 	<table class="table"><tr>
 	<th>No.</th><th>아이디</th><th>비번</th><th>이름</th><th>생년월일</th>
 	<th>성별</th><th>학년</th><th>반</th><th>점수</th><th>역할</th>
@@ -18,6 +29,7 @@
 	</tr>
 	</table>
 	</div>
+	-->
 </template>
 <script>
 import axios from 'axios'
@@ -45,4 +57,25 @@ export default{
 .table tr{border : 1px solid black}
 .table tr th{border : 1px solid black}
 .table tr td{border : 1px solid black}
+.login-form {
+    width: 340px;
+    margin: 50px auto;
+}
+.login-form form {
+    margin-bottom: 15px;
+    background: #f7f7f7;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    padding: 30px;
+}
+.login-form h2 {
+    margin: 0 0 15px;
+}
+.form-control, .btn {
+    min-height: 38px;
+    border-radius: 2px;
+}
+.btn {        
+    font-size: 15px;
+    font-weight: bold;
+}
 </style>
