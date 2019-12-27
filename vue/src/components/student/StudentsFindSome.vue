0c0ef9c1394e41471c61a0483545ab80fa2b1b0a
parkjungkwan <pakjkwan@gmail.com>
2019-12-27 15:09:03 +0900

    15:08

--- vue/src/components/student/StudentsFindSome.vue
+++ vue/src/components/student/StudentsFindSome.vue
@@ -2,13 +2,31 @@
 	<div class="login-form">
     <form>
         <h2 class="text-center">조건 검색</h2>  
-		<a @click.prevent="findSearchWord">학년별 1등과 최하위 학생</a>     
         <div class="form-group">
             <input v-model="searchWord" type="text" class="form-control" placeholder="Username" required="required">
         </div>
         <div class="form-group">
             <button @click.prevent="find" type="submit" class="btn btn-primary btn-block">전 송</button>
         </div>
+		<a @click.prevent="namesOfStudents">1. 학생의 이름</a>
+		<a @click.prevent="streamToArray">2. 스트림을 배열로 전환</a>
+		<a @click.prevent="streamToMap">3. 스트림을 맵으로 전환. 학생이름이  key</a>
+		<a @click.prevent="theNumberOfStudents">4. 학생의 수</a>
+		<a @click.prevent="totalScore">5. 학생들 총점</a>
+		<a @click.prevent="topStudent">6. 1등 학생</a>
+		<a @click.prevent="getStat">7. 전체 학생 성적 통계</a>
+		<a @click.prevent="nameList">8. 전체 학생 이름</a>
+		<a @click.prevent="partioningBy">1.단순분할 (성별로 분할)</a>
+		<a @click.prevent="partioningCountPerGender">2.단순분할 (성별 학생수)</a>
+		<a @click.prevent="partioningTopPerGender">3.단순분할 (성별 1등)</a>
+		<a @click.prevent="partioningRejectPerGender">4.다중분할 (성별 불합격자,  50점이하)</a>
+		<a @click.prevent="groupingByBan">1. 단순 그룹화(반별로 그룹화)</a>
+		<a @click.prevent="groupingByGrade">2. 단순그룹화(성적별로 그룹화)</a>
+		<a @click.prevent="groupingByCountByLevel">3. 단순그룹화 + 통계(성적별 학생수))</a>
+		<a @click.prevent="groupingByHakAndBan">4. 다중 그룹화 (학년별, 반별)</a>
+		<a @click.prevent="groupingTopByHakAndBan">5. 다중 그룹화 +통계 (학년별, 반별  1등)</a>
+		<a @click.prevent="groupingByStat">6. 다중그룹화 + 통계(학년별, 반별  성적그룹)</a>
+
     </form>
 	<!--
 	<div>학생 조건 검색결과  
